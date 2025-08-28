446a6d2c9d0d90f008a7436c3d4f0cf333df2bb9
Rey Joshua Macarat <reyjoshuamacarat@gmail.com>
2025-08-28 16:48:24 +0800

    Hello World

---
+++ src/app/_layout.tsx
@@ -0,0 +1,19 @@
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="index" />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </>
+  );
+}
